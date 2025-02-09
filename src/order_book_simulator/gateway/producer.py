a58b43bc1408b978ee23a21f83af6dc7a07ce61b
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-02-09 03:07:16 +0000

    fix: Remove unused imports

--- src/order_book_simulator/gateway/producer.py
+++ src/order_book_simulator/gateway/producer.py
@@ -1,10 +1,7 @@
 import asyncio
 import json
 import logging
-from datetime import datetime
 from typing import Any
-from uuid import UUID
-from decimal import Decimal
 
 from aiokafka import AIOKafkaProducer
 from fastapi import HTTPException
