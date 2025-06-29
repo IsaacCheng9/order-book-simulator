8cbae7a82077fa8179e9cf7122bdc8a6edac578b
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-06-30 00:56:03 +0100

    feat: Set up Streamlit in a Docker container, add basic Streamlit page

---
+++ src/order_book_simulator/ui/streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+def main():
+    """
+    Orchestrates the Streamlit app flow.
+    """
+    st.title("Order Book Simulator")
+    st.text(
+        "This is the dashboard for the Order Book Simulator, providing a visual representation of the various components of the system."
+    )
+
+
+if __name__ == "__main__":
+    main()
