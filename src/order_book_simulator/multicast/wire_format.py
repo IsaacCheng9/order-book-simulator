f8feb4d82a4872d8bcb19f3e349e52f27034fef8
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-03-10 20:09:28 +0000

    feat: Add `decode()` function for wire format

--- src/order_book_simulator/multicast/wire_format.py
+++ src/order_book_simulator/multicast/wire_format.py
@@ -12,6 +12,12 @@ HEARTBEAT = 2
 
 
 def encode(message_type: int, sequence_number: int, payload: bytes) -> bytes:
+    if len(payload) > 2**16 - 1:
+        raise ValueError(
+            f"Payload too long - needs to be less than {2**16 - 1} (2^16 - 1) bytes."
+            f"Got {len(payload)} bytes."
+        )
+
     header = struct.pack(
         HEADER_FORMAT,
         message_type,
@@ -19,3 +25,17 @@ def encode(message_type: int, sequence_number: int, payload: bytes) -> bytes:
         len(payload),
     )
     return header + payload
+
+
+def decode(data: bytes) -> tuple[int, int, bytes]:
+    if len(data) < HEADER_SIZE:
+        raise ValueError(f"Data too short - needs to be at least {HEADER_SIZE} bytes.")
+
+    message_type, sequence_number, payload_length = struct.unpack(
+        HEADER_FORMAT, data[:HEADER_SIZE]
+    )
+    return (
+        message_type,
+        sequence_number,
+        data[HEADER_SIZE : HEADER_SIZE + payload_length],
+    )
